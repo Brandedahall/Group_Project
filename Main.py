1d02ebb1014a6d49401821fc015f1750539d1ba4
Adam Plunkett <agcplunkett@googlemail.com>
2016-02-25 21:55:43 +0000

    adding class funcitonality, still early stage, shows initial dev tho

--- Main.py
+++ Main.py
@@ -11,7 +11,9 @@ Variables.UserName = input("Alright! To start us off, please can you tell me you
 
 print("It's nice to meet you, " + Variables.UserName + " To start us off, please would you answer this short question?")
 
-Variables.QuestionAnswerOne = input("Would you say that you have had a good day today?")
+#create string to hold input temp
+usrTempAns = ""
+usrTempAns = input("Would you say that you have had a good day today?")
 # We need to ask the first question here. I have added in a sample question to get us started (To be changed later!)
 
 
@@ -19,16 +21,18 @@ Variables.QuestionAnswerOne = input("Would you say that you have had a good day 
 
 exclude = set(string.punctuation)  # This strips all punctuation from the user's reply.
 
-Variables.QuestionAnswerOne = ''.join(ch for ch in Variables.QuestionAnswerOne if ch not in exclude)
-Variables.QuestionAnswerOne = Variables.QuestionAnswerOne.lower()  # This turns all uppercase characters into lower case
-Variables.QuestionAnswerOne = Variables.QuestionAnswerOne.split(" ")  # This splits the string into a list of words.
+#Variables.QuestionAnswerOne = ''.join(ch for ch in Variables.QuestionAnswerOne if ch not in exclude)
+#Variables.QuestionAnswerOne = Variables.QuestionAnswerOne.lower()  # This turns all uppercase characters into lower case
+#Variables.QuestionAnswerOne = Variables.QuestionAnswerOne.split(" ")  # This splits the string into a list of words.
 
-print(Variables.QuestionAnswerOne)  # Prints the list of the words from the user's reply.
+#print(Variables.QuestionAnswerOne)  # Prints the list of the words from the user's reply.
 
-for Word in Variables.QuestionAnswerOne:
-    if Word in Dictionary.ThingsList_General:
-        Variables.Word_General = Word
+#for Word in Variables.QuestionAnswerOne:
+   # if Word in Dictionary.ThingsList_General:
+        #Variables.Word_General = Word
 
-print(Variables.Word_General)
+ans1 = Variables.usrAns(1, usrTempAns)
+ans1.printAns
+
 # Now, with the splitting part out of the way, the process of reading each word is made easier. We want to be able to
 # read the emotional level.
--- Variables.py
+++ Variables.py
@@ -1,10 +1,37 @@
+import string
+import Dictionary
+#import files
+
 UserName = ""
 # The user's name is stored here.
 
-QuestionAnswerOne = ""
+exclude = set(string.punctuation)  # This strips all punctuation from the user's reply.
+
+#QuestionAnswerOne = ""
 # The user's answers will be stored here.
 
 Word_General = ""
 
 # We want some variables relating to a 3d graph here. The graph will need to store the plotted points for each question.
 
+#classImplementation
+class usrAns():
+    print("*******")
+    def __init__(self, quesNo, quesAns):
+        self.quesNo = quesNo
+        self.quesAns = quesAns
+
+        #does the same as where they used to be, just in a function
+        quesAns = ''.join(ch for ch in quesAns if ch not in exclude)
+        quesAns = quesAns.lower()  # This turns all uppercase characters into lower case
+        quesAns = quesAns.split(" ")  # This splits the string into a list of words.
+
+        print(quesAns)  # Prints the list of the words from the user's reply.
+
+        for Word in quesAns:
+            if Word in Dictionary.ThingsList_General:
+                Word_General = Word
+
+    def printAns():
+        print(Word_General)
+        print("This is a test")
