3a560e0b78b433df9c2fb7bc2c5c35e7aeeff859
Adam Plunkett <agcplunkett@googlemail.com>
2016-03-14 13:42:43 +0000

    added some small exception handling for general purposes, will give more indepth handling later

--- Main.py
+++ Main.py
@@ -5,11 +5,16 @@ import User
 print("Hello! I'm FAI, but please call me Fortune!")
 # Simple greeting.
 
-# Takes user response and saves username
-Variables.userName = input("Alright! To start us off, please can you tell me your name?")
-print("It's nice to meet you, " + User.Username(Variables.userName))
+# try looks for exceptions to contain
+try:
+    # Takes user response and saves username
+    Variables.userName = input("Alright! To start us off, please can you tell me your name?")
+    print("It's nice to meet you, " + User.Username(Variables.userName))
+# except to catch any errors, and return the error
+except ValueError:
+    print("shits broken")
 
-#
+
 Variables.questionAnswer = input("Would you say that you have had a good day today?")
 print("Baseline level:", User.BaseAnalyse(Variables.questionAnswer))
 
@@ -18,22 +23,26 @@ print("Baseline level:", User.BaseAnalyse(Variables.questionAnswer))
 
 # A way to navigate through the tpoic questions
 # We will need to include a way of collecting keyword the user inputs for each topic. Hence the separate if statements
-while Variables.topic < 5:
-    if Variables.topic == 0:
-        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexJob))
-        User.Analyse(Variables.questionAnswer, Variables.topic)
-        Variables.topicIndexJob -= 2
-    if Variables.topic == 1:
-        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexFam))
-        User.Analyse(Variables.questionAnswer, Variables.topic)
-        Variables.topicIndexFam -= 2
-    if Variables.topic == 2:
-        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexLove))
-        User.Analyse(Variables.questionAnswer, Variables.topic)
-        Variables.topicIndexLove -= 2
-    if Variables.topic == 3:
-        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexFnd))
-        User.Analyse(Variables.questionAnswer, Variables.topic)
-        Variables.topicIndexFnd -= 2
+# try looks for exceptions to contain
+try:
+    while Variables.topic < 5:
+        if Variables.topic == 0:
+            Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexJob))
+            User.Analyse(Variables.questionAnswer, Variables.topic)
+            Variables.topicIndexJob -= 2
+        if Variables.topic == 1:
+            Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexFam))
+            User.Analyse(Variables.questionAnswer, Variables.topic)
+            Variables.topicIndexFam -= 2
+        if Variables.topic == 2:
+            Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexLove))
+            User.Analyse(Variables.questionAnswer, Variables.topic)
+            Variables.topicIndexLove -= 2
+        if Variables.topic == 3:
+            Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexFnd))
+            User.Analyse(Variables.questionAnswer, Variables.topic)
+            Variables.topicIndexFnd -= 2
+except ValueError:
+    print("This shit aint gonna fly")
 
 
