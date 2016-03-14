88162a290653df1a5081f9744a310c7a503a7a3e
BenBywater <benbywater1@gmail.com>
2016-03-14 19:14:43 +0000

    More bug fixes
    The emotional value for values previously popped now keep their initial value

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Variables
 import User
 # Imports the variables file.
@@ -37,3 +38,44 @@ while Variables.topic < 5:
         Variables.topicIndexFnd -= 2
 
 
+=======
+import Variables
+import User
+# Imports the variables file.
+
+print("Hello! I'm FAI, but please call me Fortune!")
+# Simple greeting.
+
+# Takes user response and saves username
+Variables.userName = input("Alright! To start us off, please can you tell me your name?")
+print("It's nice to meet you, " + User.Username(Variables.userName))
+
+#
+Variables.questionAnswer = input("Would you say that you have had a good day today?")
+print("Baseline level:", User.BaseAnalyse(Variables.questionAnswer))
+
+# Variables.questionAnswer = input("Hmm... okay then! Would you say that work has been on your mind recently?")
+# print("Current emotional level:", + User.analyse(Variables.questionAnswer, Variables.topics[Variables.topic]))
+
+# A way to navigate through the tpoic questions
+# We will need to include a way of collecting keyword the user inputs for each topic. Hence the separate if statements
+while Variables.topic < 5:
+    if Variables.topic == 0:
+        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexJob))
+        User.Analyse(Variables.questionAnswer, Variables.topic)
+        Variables.topicIndexJob -= 2
+    if Variables.topic == 1:
+        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexFam))
+        User.Analyse(Variables.questionAnswer, Variables.topic)
+        Variables.topicIndexFam -= 2
+    if Variables.topic == 2:
+        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexLove))
+        User.Analyse(Variables.questionAnswer, Variables.topic)
+        Variables.topicIndexLove -= 2
+    if Variables.topic == 3:
+        Variables.questionAnswer = input(Variables.t.pop(Variables.topicIndexFnd))
+        User.Analyse(Variables.questionAnswer, Variables.topic)
+        Variables.topicIndexFnd -= 2
+
+
+>>>>>>> origin/master
--- User.py
+++ User.py
@@ -1,3 +1,94 @@
+<<<<<<< HEAD
+import string
+import Variables
+import AI
+
+# Takes user input and saves their username
+def Username(userName):
+    # split string into list
+    Variables.Name = Variables.userName.split(" ")
+    if 'I' in Variables.Name:
+        Variables.Name.remove('I')
+    # If user inout is larger than one word
+    if len(Variables.Name) >= 2:
+        # Search through the list of names and appends username
+        for word in Variables.Name:
+            for name in Variables.names:
+                if word == name:
+                    Variables.TrueName.append(word)
+        # If user name has used a capital letter then it is appended
+        for x in Variables.Name:
+            if x.istitle():
+                if x == "My":
+                    print("")
+                else:
+                    Variables.TrueName.append(x)
+        Variables.Name.clear()
+        Variables.Name = Variables.TrueName.pop()
+    # If user has entered jsut their name
+    if len(Variables.Name) == 1:
+        Variables.Name = Variables.Name.pop()
+    # If user has entered nothing then their name is set to Anonymous
+    if len(Variables.Name) == 0:
+        print("I see... well, since you didn't enter a name, I'll call you Anonymous!")
+        Variables.Name = "Anonymous"
+    return Variables.Name
+
+# analyses users response and updates current emotional level
+def Analyse(response, topic):
+    exclude = set(string.punctuation)  # This strips all punctuation from the user's reply.
+    # breaks down user response and returns emotional level
+    Variables.questionAnswer = response
+    Variables.questionAnswer = ''.join(ch for ch in Variables.questionAnswer if ch not in exclude)
+    Variables.questionAnswer = Variables.questionAnswer.lower()  # This turns all uppercase characters into lower.
+    Variables.questionAnswer = Variables.questionAnswer.split(" ")  # This splits the string into a list of words.
+    for x in Variables.questionAnswer:
+        if x in Variables.words:
+            Variables.index = Variables.words.index(x)
+            # exception handling allows us to pop a values and then insert it back into the list to be used again
+            try:
+                #Variables.emotion += int(Variables.words.pop(Variables.index + 1))
+               # Variables.words.insert(Variables.index+1, 0)
+                Variables.instanceEmotion = int(Variables.words.pop(Variables.index + 1))
+                Variables.emotion += Variables.instanceEmotion
+                Variables.words.insert(Variables.index+1, Variables.instanceEmotion)
+            except ValueError:
+
+                Variables.instanceEmotion = int(Variables.words.pop(Variables.index))
+                Variables.emotion += Variables.instanceEmotion
+                Variables.words.insert(Variables.index-1, Variables.instanceEmotion)
+        if x not in Variables.words:
+            Variables.emotion += 0
+    # update current emotional level from user response
+    Variables.currentEmotion += Variables.emotion
+    print(Variables.currentEmotion)
+    Variables.topic = AI.nexttopic(Variables.currentEmotion, topic, Variables.baseline)
+    Variables.emotion = 0
+    return Variables.currentEmotion, Variables.topic
+
+# analyses the baseline response and establishes the baseline emotional level
+def BaseAnalyse(response):
+    exclude = set(string.punctuation)  # This strips all punctuation from the user's reply.
+
+    Variables.questionAnswer = response
+    Variables.questionAnswer = ''.join(ch for ch in Variables.questionAnswer if ch not in exclude)
+    Variables.questionAnswer = Variables.questionAnswer.lower()  # This turns all uppercase characters into lower.
+    Variables.questionAnswer = Variables.questionAnswer.split(" ")  # This splits the string into a list of words.
+    for x in Variables.questionAnswer:
+        if x in Variables.words:
+            Variables.index = Variables.words.index(x)
+            # exception handling allows us to pop a values and then insert it back into the list to be used again
+            try:
+                Variables.emotion += int(Variables.words.pop(Variables.index + 1))
+                Variables.words.insert(Variables.index+1, 0)
+            except ValueError:
+                Variables.emotion += int(Variables.words.pop(Variables.index))
+                Variables.words.insert(Variables.index-1, 0)
+        if x not in Variables.words:
+            Variables.emotion += 0
+    # establishes emotional baseline
+    Variables.baseline += Variables.emotion
+=======
 import string
 import Variables
 import AI
@@ -82,4 +173,5 @@ def BaseAnalyse(response):
             Variables.emotion += 0
     # establishes emotional baseline
     Variables.baseline += Variables.emotion
+>>>>>>> origin/master
     return Variables.baseline
